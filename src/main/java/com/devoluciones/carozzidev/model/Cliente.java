e85ddaedbf5accf51778fdc5c92c53fbe83a0cb9
albert <correo@albert.cl>
2023-12-26 00:04:04 -0300

    Clase Cliente, comentarios, validaciones, uso de constantes para nombres de columnas.

--- src/main/java/com/devoluciones/carozzidev/model/Cliente.java
+++ src/main/java/com/devoluciones/carozzidev/model/Cliente.java
@@ -2,8 +2,15 @@ package com.devoluciones.carozzidev.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+
+
+
+/**
+ * Clase que representa a un cliente en el sistema
+ */
 @Entity
 @Getter
 @Setter
@@ -12,14 +19,32 @@ import lombok.*;
 @ToString
 public class Cliente {
 
+    /**
+     * Utilización de constantes, esto facilita la refactorización si los nombres de las columnas cambian.
+     */
+    public static final String ID_COLUMN = "id_cliente";
+    public static final String NOMBRE_COLUMN = "nombre_cliente";
+    public static final String RUT_COLUMN = "rut_cliente";
+
+    /**
+     * Identificador único del cliente.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_cliente")
+    @Column(name = ID_COLUMN)
     private Long idCliente;
 
-    @Column(name = "nombre_cliente")
+    /**
+     * Nombre del cliente.
+     */
+    @Column(name = NOMBRE_COLUMN)
+    @NotBlank(message = "El nombre del cliente no puede estar en blanco")
     private String nombreCliente;
 
-    @Column(name = "rut_cliente")
+    /**
+     * Rut (Rol Único Tributario) del cliente.
+     */
+    @Column(name = RUT_COLUMN)
+    @NotBlank(message = "El rut del cliente no puede estar en blanco")
     private String rutCliente;
 }
